c5ee639e064a37a3a091eeef6b89d007077375f1
Panuwat Chaiwongthon <panuwat.c@conicle.com>
2023-01-20 23:12:27 +0700

    Implement sign up

--- authentication/app/handler/authentication.go
+++ authentication/app/handler/authentication.go
@@ -14,7 +14,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func CreateAccountWitEmail(db *mongo.Database, res http.ResponseWriter, req *http.Request) {
+func SignUpAccountWithEmail(db *mongo.Database, res http.ResponseWriter, req *http.Request) {
 
 	account := new(model.Account)
 
@@ -65,5 +65,98 @@ func CreateAccountWitEmail(db *mongo.Database, res http.ResponseWriter, req *htt
 	}
 
 	ResponseWriter(res, http.StatusCreated, "", map[string]interface{}{"accessToken": accessToken})
+
+}
+
+func SignInAccountWithEmail(db *mongo.Database, res http.ResponseWriter, req *http.Request) {
+
+	signIn := new(model.SignInWithEmail)
+
+	err := json.NewDecoder(req.Body).Decode(signIn)
+	if err != nil {
+		ResponseWriter(res, http.StatusBadRequest, "Body json request have issues!!!", nil)
+		return
+	}
+
+	if ok, errors := core.ValidateInputs(signIn); !ok {
+		model.NewValidatedResponse(http.StatusBadRequest, "Invalid is value please check again.", errors)
+		return
+	}
+
+	collection := db.Collection("account")
+
+	var account model.Account
+	findOptions := options.FindOne().SetProjection(bson.D{primitive.E{Key: "password", Value: 1}})
+	err = collection.FindOne(context.TODO(), bson.D{primitive.E{Key: "email", Value: signIn.Email}, primitive.E{Key: "social", Value: bson.M{"$exists": false}}}, findOptions).Decode(&account)
+	if err != nil {
+		ResponseWriter(res, http.StatusBadRequest, "Account not found. Please check again.", nil)
+		return
+
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(signIn.Password))
+
+	if err != nil {
+		ResponseWriter(res, http.StatusBadRequest, "Password is not match.", nil)
+		return
+	}
+
+	accessToken, err := core.GenerateJWT(map[string]interface{}{"_id": account.ID})
+
+	if err != nil {
+		ResponseWriter(res, http.StatusBadRequest, "Invalid authentication token.", nil)
+		return
+	}
+
+	ResponseWriter(res, http.StatusOK, "", map[string]interface{}{"accessToken": accessToken})
+
+}
+
+func SignInAccountWithSocial(db *mongo.Database, res http.ResponseWriter, req *http.Request) {
+
+	account := new(model.AccountSyncSocial)
+
+	err := json.NewDecoder(req.Body).Decode(account)
+	if err != nil {
+		ResponseWriter(res, http.StatusBadRequest, "Body json request have issues!!!", nil)
+		return
+	}
+
+	if ok, errors := core.ValidateInputs(account); !ok {
+		model.NewValidatedResponse(http.StatusBadRequest, "Invalid is value please check again.", errors)
+		return
+	}
+
+	collection := db.Collection("account")
+
+	findOptions := options.FindOne().SetProjection(bson.D{primitive.E{Key: "email", Value: 1}})
+	err = collection.FindOne(context.TODO(), bson.D{primitive.E{Key: "email", Value: account.Email}, primitive.E{Key: "social.socialId", Value: account.Social.SocialId}}, findOptions).Decode(&account)
+	if err != nil {
+		_, err = collection.InsertOne(context.TODO(), model.NewAccountSyncSocial(account))
+		if err != nil {
+			mongoException := err.(mongo.WriteException)
+			if mongoException.WriteErrors[0].Code == 11000 {
+				ResponseWriter(res, http.StatusBadRequest, "Account is existing, Please try again.", nil)
+			} else {
+				ResponseWriter(res, http.StatusBadRequest, err.Error(), nil)
+			}
+			return
+		}
+
+		err = collection.FindOne(context.TODO(), bson.D{primitive.E{Key: "email", Value: account.Email}}, findOptions).Decode(&account)
+		if err != nil {
+			ResponseWriter(res, http.StatusBadRequest, "Account does not exist.", nil)
+			return
+		}
+	}
+
+	accessToken, err := core.GenerateJWT(map[string]interface{}{"_id": account.ID})
+
+	if err != nil {
+		ResponseWriter(res, http.StatusBadRequest, "Invalid authentication token.", nil)
+		return
+	}
+
+	ResponseWriter(res, http.StatusOK, "", map[string]interface{}{"accessToken": accessToken})
 
 }
--- authentication/app/model/account.go
+++ authentication/app/model/account.go
@@ -4,16 +4,10 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type (
-	RegisterWithEmailDto struct {
-		Email       string `json:"email" validate:"required,email"`
-		Phone       string `json:"phone" validate:"required"`
-		Password    string `json:"password" validate:"required"`
-		DisplayName string `json:"displayName" validate:"required"`
-	}
-
 	Account struct {
 		ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 		Email       string             `json:"email,omitempty" bson:"email,omitempty"  validate:"required,email"`
@@ -22,6 +16,26 @@ type (
 		DisplayName string             `json:"displayName,omitempty" bson:"displayName,omitempty" validate:"required"`
 		CreatedAt   time.Time          `json:"createdAt" bson:"createdAt,omitempty"`
 		UpdateAt    time.Time          `json:"updatedAt" bson:"updatedAt,omitempty"`
+	}
+
+	AccountSyncSocial struct {
+		ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+		Email       string             `json:"email,omitempty" bson:"email,omitempty"  validate:"required,email"`
+		DisplayName string             `json:"displayName,omitempty" bson:"displayName,omitempty" validate:"required"`
+		Social      Social             `json:"social" bson:"social"`
+		CreatedAt   time.Time          `json:"createdAt" bson:"createdAt,omitempty"`
+		UpdateAt    time.Time          `json:"updatedAt" bson:"updatedAt,omitempty"`
+	}
+	Social struct {
+		Email       string `json:"email,omitempty" bson:"email,omitempty"  validate:"required,email"`
+		Provider    string `json:"provider,omitempty" bson:"provider,omitempty" validate:"required"`
+		SocialId    string `json:"socialId,omitempty" bson:"socialId,omitempty" validate:"required"`
+		AccessToken string `json:"accessToken,omitempty" bson:"accessToken,omitempty" validate:"required"`
+	}
+
+	SignInWithEmail struct {
+		Email    string `json:"email,omitempty" bson:"email,omitempty"  validate:"required,email"`
+		Password string `json:"password,omitempty" bson:"password,omitempty"`
 	}
 )
 
@@ -34,4 +48,23 @@ func NewAccount(account *Account) *Account {
 		CreatedAt:   time.Now(),
 		UpdateAt:    time.Now(),
 	}
+}
+
+func NewAccountSyncSocial(account *AccountSyncSocial) *AccountSyncSocial {
+	return &AccountSyncSocial{
+		Email:       account.Email,
+		DisplayName: account.DisplayName,
+		Social:      account.Social,
+		CreatedAt:   time.Now(),
+		UpdateAt:    time.Now(),
+	}
+}
+
+func (account *Account) GenerateFromPassword(password string) error {
+	newPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+	account.Password = string(newPassword)
+	return nil
 }
